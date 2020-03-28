b03cffde513cdd583cf424f3189de25d54f34d6c
Robson Peacock <robson.peacock@leesmanindex.com>
2020-03-28 21:48:41 +0000

    Add Category Products association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200328213451) do
+ActiveRecord::Schema.define(version: 20200328214101) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20200328213451) do
     t.string   "expiry_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
   end
 
+  add_foreign_key "products", "categories"
 end
